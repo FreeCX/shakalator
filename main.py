8c2ba59ab036704aadfea6919e81e94fc8482b16
Alexey Golubev <dr.freecx@gmail.com>
2017-07-30 20:54:17 +0300

    [add] pdf delete

--- main.py
+++ main.py
@@ -1,20 +1,49 @@
 from flask import Flask, render_template, redirect, url_for
 from flask import request, jsonify, send_file
+from multiprocessing import Process, Pipe
 from werkzeug import secure_filename
 from os import path, system, remove
-from multiprocessing import Process
 from hashlib import md5
 import time
+
+
+# 5m
+TIME_DELTA = 5 * 60
+
 
 """ инициализируем flask """
 app = Flask(__name__, static_url_path='')
 """ настраиваем папку для загрузки файлов """
 app.config['UPLOAD_FOLDER'] = path.join(path.dirname(path.realpath(__file__)), 'upload')
 
+
 def get_hash(text):
     return md5(text.encode('utf-8')).hexdigest()
 
-""" обрабатываем файл """
+
+# главный поток обработки
+def handler(conn):
+    clean_list = []
+    while True:
+        if conn.poll(10):
+            data = conn.recv()
+            sh_thread = Process(target=gs_shakalizing, args=data)
+            sh_thread.start()
+            sh_thread.join()
+            clean_list.append((data[1], time.clock_gettime(0)))
+        curr_time = time.clock_gettime(0)
+        new_list = []
+        for (filename, l_time) in clean_list:
+            delta = curr_time - l_time
+            if delta > TIME_DELTA:
+                gen_file = path.join(app.config['UPLOAD_FOLDER'], filename)
+                remove(gen_file)
+            else:
+                new_list.append((filename, l_time))
+        clean_list = new_list
+
+
+# обрабатываем файл
 def gs_shakalizing(filename, new_name, dpi):
     old_file = path.join(app.config['UPLOAD_FOLDER'], filename)
     new_file = path.join(app.config['UPLOAD_FOLDER'], new_name)
@@ -24,7 +53,8 @@ def gs_shakalizing(filename, new_name, dpi):
     system(cmd)
     remove(old_file)
 
-""" проверяем статус обработки """
+
+# проверяем статус обработки
 @app.route('/process', methods=['POST', 'GET'])
 def process():
     if request.method == 'POST':
@@ -41,12 +71,13 @@ def process():
         filename = request.values.get('code')
         if filename:
             file_path = path.join(app.config['UPLOAD_FOLDER'], filename)
-            return send_file(file_path, mimetype='application/pdf', 
-                as_attachment=True, attachment_filename=filename + '.pdf')
+            return send_file(file_path, mimetype='application/pdf',
+                             as_attachment=True, attachment_filename=filename + '.pdf')
         else:
             return 404
 
-""" загрузка файла и страница с ожиданием """
+
+# загрузка файла и страница с ожиданием
 @app.route('/shakalazing', methods=['POST', 'GET'])
 def shakalazing():
     if request.method == 'POST':
@@ -55,19 +86,24 @@ def shakalazing():
             filename = secure_filename(file.filename)
             file.save(path.join(app.config['UPLOAD_FOLDER'], filename))
             dpi = request.form.get('dpi')
-            hash_code = get_hash(filename)
-            sh_thread = Process(target=gs_shakalizing, args=(filename, hash_code, dpi))
-            sh_thread.start()
+            hash_code = get_hash(filename + time.strftime('%H:%M:%S'))
+            parent_conn.send((filename, hash_code, dpi))
             return redirect(url_for('shakalazing', code=hash_code))
         else:
             return redirect(url_for('index'))
     else:
         return render_template('shakalazing.html', code=request.values.get('code'))
 
-""" главная страница """
+
+# главная страница
 @app.route('/')
 def index():
     return render_template('index.html')
 
+
 if __name__ == '__main__':
-    app.run()
+    parent_conn, child_conn = Pipe()
+    main_proc = Process(target=handler, args=(child_conn,))
+    main_proc.start()
+    app.run(host='0.0.0.0', port='5000')
+    main_proc.join()
